06ed9a42be727d09bfbe35a35ac0ccff5dd8aa72
dmazurek <dmazurek@olx.pl>
2018-04-15 13:40:33 +0200

    full coverage for response objects

---
+++ tests/unit/Response/JsonRpcResponseCollectionTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace DawidMazurek\JsonRpcClient\Response;
+
+use DawidMazurek\JsonRpcClient\Request\JsonRpcRequest;
+use DawidMazurek\JsonRpcClient\Request\JsonRpcRequestCollection;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class JsonRpcResponseCollectionTest extends TestCase
+{
+    /**
+     * @var MockObject|JsonRpcRequestCollection
+     */
+    private $requests;
+
+    /**
+     * @var MockObject|JsonRpcRequest
+     */
+    private $request;
+
+    public function setUp()
+    {
+        $this->requests = $this->createMock(JsonRpcRequestCollection::class);
+        $this->request = $this->createMock(JsonRpcRequest::class);
+    }
+
+    /**
+     * @test
+     */
+    public function returnsResponseForGivenRequest()
+    {
+        $response = $this->createMock(JsonRpcResponse::class);
+        $response->method('getId')->willReturn(1);
+        $this->requests->method('getRequestId')->willReturn(1);
+
+        $responses = new JsonRpcResponseCollection($this->requests);
+        $responses->addResponse($response);
+
+        $this->assertSame($response, $responses->getResponseFor($this->request));
+    }
+
+    /**
+     * @test
+     */
+    public function detectsFailedResponseWhenErrorObjectGiven()
+    {
+        $response = $this->createMock(JsonRpcRequestError::class);
+        $response->method('getId')->willReturn(1);
+        $this->requests->method('getRequestId')->willReturn(1);
+
+        $responses = new JsonRpcResponseCollection($this->requests);
+        $responses->addResponse($response);
+
+        $this->assertTrue($responses->hasRequestFailed($this->request));
+    }
+
+    /**
+     * @test
+     */
+    public function notDetectsFailedResponseWhenResponseObjectGiven()
+    {
+        $response = $this->createMock(JsonRpcRequestResponse::class);
+        $response->method('getId')->willReturn(1);
+        $this->requests->method('getRequestId')->willReturn(1);
+
+        $responses = new JsonRpcResponseCollection($this->requests);
+        $responses->addResponse($response);
+
+        $this->assertFalse($responses->hasRequestFailed($this->request));
+    }
+}
